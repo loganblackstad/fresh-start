f18167afa973f3d00ceae3268ec53725b34c31dc
Logan Blackstad <logan.blackstad@gmail.com>
2020-04-01 14:56:41 -0400

    add more practice scripts for DC class work

---
+++ degree_conversion.py
@@ -0,0 +1,3 @@
+c = input("Temperature in C?")
+f = 9 / 5 * int(c) + 32
+print("{} F".format(f))
---
+++ work_or_sleep_in.py
@@ -0,0 +1,6 @@
+day = int(input("Day (0-6)?"))
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+if day == 0 or day == 6:
+    print("Sleep in")
+else:
+    print("Go to work")
